4a912f9529c7a027b885d400e38657317a34fc6e
zte-majiang <36257856+zte-majiang@users.noreply.github.com>
2019-05-12 15:43:08 +0800

    used version

--- test.cpp
+++ test.cpp
@@ -1,84 +1,7 @@
-
-
-
- 
- 
- 
- struct SubSTInfo
- {
-     bool isBlock{false};
-     uint64_t ipPort{0};
- };
- 
- typedef std::map<SessionType, SubSTInfo> SubSessionTypeInfo;
- typedef std::map<SessionType, StInfo> SessionTypeInfo;
- 
-
-./pub-sub/common/include/common/PubSubConst.h:44:typedef std::string SessionId;  // SessionId = SessionType + "/" + SessionKey
-./pub-sub/common/include/common/PubSubConst.h:45:typedef std::string SessionType;
-./pub-sub/common/include/common/PubSubConst.h:46:typedef std::string SessionKey;
-
-
- 
-=================================================================
 #include <benchmark/benchmark.h>
 #include <array>
+#include <memory>
 
-constexpr int len = 6;
-
-// constexpr function具有inline属性，你应该把它放在头文件中
-constexpr auto my_pow(const int i)
-{
-    return i * i;
-}
-
-// 使用operator[]读取元素，依次存入1-6的平方
-static void bench_array_operator(benchmark::State& state)
-{
-    std::array<int, len> arr;
-    constexpr int i = 1;
-    for (auto _: state) {
-        arr[0] = my_pow(i);
-        arr[1] = my_pow(i+1);
-        arr[2] = my_pow(i+2);
-        arr[3] = my_pow(i+3);
-        arr[4] = my_pow(i+4);
-        arr[5] = my_pow(i+5);
-    }
-}
-BENCHMARK(bench_array_operator);
-
-// 使用at()读取元素，依次存入1-6的平方
-static void bench_array_at(benchmark::State& state)
-{
-    std::array<int, len> arr;
-    constexpr int i = 1;
-    for (auto _: state) {
-        arr.at(0) = my_pow(i);
-        arr.at(1) = my_pow(i+1);
-        arr.at(2) = my_pow(i+2);
-        arr.at(3) = my_pow(i+3);
-        arr.at(4) = my_pow(i+4);
-        arr.at(5) = my_pow(i+5);
-    }
-}
-BENCHMARK(bench_array_at);
-
-// std::get<>(array)是一个constexpr function，它会返回容器内元素的引用，并在编译期检查数组的索引是否正确
-static void bench_array_get(benchmark::State& state)
-{
-    std::array<int, len> arr;
-    constexpr int i = 1;
-    for (auto _: state) {
-        std::get<0>(arr) = my_pow(i);
-        std::get<1>(arr) = my_pow(i+1);
-        std::get<2>(arr) = my_pow(i+2);
-        std::get<3>(arr) = my_pow(i+3);
-        std::get<4>(arr) = my_pow(i+4);
-        std::get<5>(arr) = my_pow(i+5);
-    }
-}
-BENCHMARK(bench_array_get);
 
 struct StInfo
  {
@@ -121,6 +44,22 @@ struct Session
  {
  }
 
+#include <random>
+#include <string>
+
+std::string random_string()
+{
+     std::string str("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+
+     std::random_device rd;
+     std::mt19937 generator(rd());
+
+     std::shuffle(str.begin(), str.end(), generator);
+
+     return str.substr(0, 32);    // assumes 32 < number of characters in str         
+}
+
+//std::string rstr = random_string();
 
 static void map_insert(benchmark::State& state)
 {
@@ -130,9 +69,14 @@ static void map_insert(benchmark::State& state)
     
     int i = 1;
     for (auto _: state) {
-    SessionKey key = 
-    Session session = std::make_shared<Session>(type, key, peerNumServletId);
+    state.PauseTiming();
+    SessionKey key = random_string();
+    state.ResumeTiming();
+    auto session = std::make_shared<Session>(type, key, peerNumServletId);
     sessions.insert(std::make_pair(key, session));
     }
 }
+
+BENCHMARK(map_insert);
 BENCHMARK_MAIN();
+
